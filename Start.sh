ab063a341b6f2d24cff1b828443ef2d28a18a7f4
hallowf <alexandre.cortegaca@gmail.com>
2018-05-27 17:41:04 +0100

    Added files to start

--- Start.sh
+++ Start.sh
@@ -2,5 +2,7 @@ chmod +x deps_suricata.sh
 chmod +x installSuricata.sh
 chmod +x startSuricata.sh
 chmod +x rm_makeSuricata.sh
+chmod +x rm_checkSuricata.sh
+chmod +x tail_suricata.sh
 
 ./deps_suricata.sh
