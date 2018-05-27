2a2986b6f1fbdef9b26860b03e6f84433d792ed6
hallowf <alexandre.cortegaca@gmail.com>
2018-05-27 17:42:18 +0100

    Cd out of directory to remove uneccessary folders

--- installSuricata.sh
+++ installSuricata.sh
@@ -1,10 +1,13 @@
 wget http://www.openinfosecfoundation.org/download/suricata-4.0.4.tar.gz
+
 tar -xvzf suricata-4.0.4.tar.gz
 cd suricata-4.0.4
 
 ./configure --enable-nfqueue --prefix=/usr --sysconfdir=/etc --localstatedir=/var
 make
 sudo checkinstall
+
+cd ..
 
 sudo rm -r suricata-4.0.4.tar.gz
 sudo rm -r suricata-4.0.4
