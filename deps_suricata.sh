560bf72d336a946e5e9d394b67bfda0d08cce087
hallowf <alexandre.cortegaca@gmail.com>
2018-05-27 17:41:55 +0100

    added checkinstall to dependencies

--- deps_suricata.sh
+++ deps_suricata.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev \
 build-essential autoconf automake libtool libpcap-dev libnet1-dev \
 libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libmagic-dev libcap-ng-dev \
-libjansson-dev pkg-config libnetfilter-queue-dev
+libjansson-dev pkg-config libnetfilter-queue-dev checkinstall
 
 ./installSuricata.sh
