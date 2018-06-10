7a22387fd04e2eb10b4c7d056b9db7014b05a830
hallowf <alexandre.cortegaca@gmail.com>
2018-06-11 00:57:37 +0100

    Updated index custom scripts

--- Web/Js/index_html.js
+++ Web/Js/index_html.js
@@ -1,14 +1,52 @@
 
 
-function Testbutton()
+function installButton()
 {
   const button = $("#Install").click(function ()
   {
-    $.get("/Shell/ls.sh", function (data)
+    $.get("/Shell/scripts/Start.sh", function (data)
+    {
+      console.log("Starting install")
+    })
+    $.get("/Shell/scripts/deps_suricata.sh", function (data)
+    {
+      console.log("Installing all dependencies")
+    })
+    $.get("/Shell/scripts/installSuricata.sh", function (data)
+    {
+      console.log("Runnign install script")
+    })
+  })
+}
+
+function testButton()
+{
+  const button = $("#Test").click(function ()
+  {
+    $.get("/Shell/scripts/ls.sh", function (data)
+    {
+      console.log("Testing")
+    })
+  })
+}
+
+function controlCenter()
+{
+  const button = $("#Run").click(function ()
+  {
+    $.get("/Shell/scripts/startSuricata.sh", function (data)
     {
       console.log("Starting install")
     })
   })
 }
 
-Testbutton()
+
+
+
+
+installButton()
+
+testButton()
+
+controlCenter()
