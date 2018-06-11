1fdfccde409d6b0b1b25589b1f1028a4e4625569
hallowf <alexandre.cortegaca@gmail.com>
2018-06-11 20:10:14 +0100

    Updated custom scripts

--- Web/Js/index_html.js
+++ Web/Js/index_html.js
@@ -1,18 +1,19 @@
 
+// Shell calls
 
 function installButton()
 {
   const button = $("#Install").click(function ()
   {
-    $.get("/Shell/scripts/Start.sh", function (data)
+    $.get("/Shell/scripts/Start.sh", function ()
     {
       console.log("Starting install")
     })
-    $.get("/Shell/scripts/deps_suricata.sh", function (data)
+    $.get("/Shell/scripts/deps_suricata.sh", function ()
     {
       console.log("Installing all dependencies")
     })
-    $.get("/Shell/scripts/installSuricata.sh", function (data)
+    $.get("/Shell/scripts/installSuricata.sh", function ()
     {
       console.log("Runnign install script")
     })
@@ -23,30 +24,75 @@ function testButton()
 {
   const button = $("#Test").click(function ()
   {
-    $.get("/Shell/scripts/ls.sh", function (data)
+    $.get("/Shell/scripts/ls.sh", function ()
     {
       console.log("Testing")
     })
   })
 }
 
+// Control center
+
 function controlCenter()
 {
   const button = $("#Run").click(function ()
   {
-    $.get("/Shell/scripts/startSuricata.sh", function (data)
+    $.get("/Shell/scripts/startSuricata.sh", function ()
     {
       console.log("Starting install")
     })
   })
 }
 
+// Iframe clean button
+
+function cleanButton()
+{
+  const button = $("#logClean").click(function ()
+  {
+    $.get("/Shell/clean", function()
+    {
+      console.log("Cleaning logs")
+    })
+  })
+}
+
+// Test buttons
+
+function testScriptButton()
+{
+  const button = $("#lsTest").click(function ()
+  {
+    $.get("/Shell/tests/ls.sh", function ()
+    {
+      console.log("Starting ls test")
+    })
+  })
+}
+
+function testErrorButton()
+{
+  const button = $("#cdTest").click(function ()
+  {
+    $.get("/Shell/tests/cd.sh", function ()
+    {
+      console.log("Starting cd test")
+    })
+  })
+}
 
 
 
+// Calling all functions
 
 installButton()
 
 testButton()
 
 controlCenter()
+
+cleanButton()
+
+testScriptButton()
+
+testErrorButton()
