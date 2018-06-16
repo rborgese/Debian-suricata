87ea8b8295a5dd8c2e50c8f8c23377b2aab81183
hallowf <alexandre.cortegaca@gmail.com>
2018-06-16 14:53:36 +0100

    Updated Start shell script to add permissions to the new test and added comments

--- Shell/scripts/Start.sh
+++ Shell/scripts/Start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-chmod u+x Shell/deps_suricata.sh
-chmod u+x Shell/installSuricata.sh
-chmod u+x Shell/startSuricata.sh
-chmod u+x Shell/rm_makeSuricata.sh
-chmod u+x Shell/rm_checkSuricata.sh
-chmod u+x Shell/tail_suricata.sh
+
+# Required scripts
+chmod u+x Shell/scripts/deps_suricata.sh
+chmod u+x Shell/scripts/installSuricata.sh
+chmod u+x Shell/scripts/startSuricata.sh
+chmod u+x Shell/scripts/rm_makeSuricata.sh
+chmod u+x Shell/scripts/rm_checkSuricata.sh
+chmod u+x Shell/scripts/tail_suricata.sh
+
+# Tests
+chmod u+x Shell/tests/bad_cd.sh
+chmod u+x Shell/tests/cd.sh
+chmod u+x Shell/tests/ls.sh
+chmod u+x Shell/tests/sudoTest.sh
+
 
 # ./Shell/deps_suricata.sh
