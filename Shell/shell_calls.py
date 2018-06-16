94847609b3efc4bd02990dd503a5e3db53adc3e1
hallowf <alexandre.cortegaca@gmail.com>
2018-06-16 14:53:01 +0100

    Updated shell calls to output stdout and stderr to text files and shell_out.html

--- Shell/shell_calls.py
+++ Shell/shell_calls.py
@@ -6,7 +6,9 @@ import os
 def clean_log(path):
     if os.path.isfile(path):
         os.remove(path)
+        open(path, "w")
     else:
+        open(path, "w")
         print("File does not exist")
 
 
@@ -48,13 +50,10 @@ def reset_soup():
 def call_start():
     out_path = "Outputs/Start.txt"
     clean_log(out_path)
-    output = "Adding execute permissions"
-    subprocess.run(["./Shell/scripts/Start.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    output = subprocess.run(["./Shell/scripts/Start.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     generic_soup("Adding execute permissions")
     if not output.stderr:
-        for line in output.stdout.splitlines():
-            new_line_txt = line + "\n"
-            output_log(new_line_txt, out_path)
+        generic_soup("Success")
     if output.stderr:
         for line in output.stderr.splitlines():
             new_line_txt = line + "\n"
@@ -64,14 +63,16 @@ def call_start():
 def call_deps():
     out_path = "Outputs/deps.txt"
     clean_log(out_path)
-    output = subprocess.run(["./Shell/scripts/deps_suricata.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    out_array = str(output.stdout).split()
     generic_soup("Installing all necessary dependencies, creating log file at: {}".format(out_path))
+    generic_soup("This might take a while")
+    output = subprocess.run(["./Shell/scripts/deps_suricata.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     if not output.stderr:
+        generic_soup("All requirements met")
         for line in output.stdout.splitlines():
             new_line_txt = line + "\n"
             output_log(new_line_txt, out_path)
     if output.stderr:
+        generic_soup("An error ocurred, please check the log file at {}, the error will also be displayed below".format(out_path))
         for line in output.stderr.splitlines():
             new_line_txt = line + "\n"
             generic_soup("Error: " + line)
@@ -80,10 +81,11 @@ def call_deps():
 def call_install():
     out_path = "Outputs/install.txt"
     clean_log(out_path)
-    output = suprocess.run(["./Shell/scripts/installSuricata.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    out_array = str(output.stdout).split()
     generic_soup("Making and installing, creating log file at: {}".format(out_path))
+    generic_soup("This might also take a while")
+    output = suprocess.run(["./Shell/scripts/installSuricata.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     if not output.stderr:
+        generic_soup("It seems everything went ok, however it is recommended to run Simple Test")
         for line in output.stdout.splitlines():
             new_line_txt = line + "\n"
             output_log(new_line_txt, out_path)
@@ -139,8 +141,3 @@ def call_bad_cd():
             new_line_txt = line + "\n"
             generic_soup("Error: " + line)
             output_log(new_line_txt, out_path)
-
-
-call_ls()
-call_cd()
-call_bad_cd()
