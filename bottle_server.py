5a2acad76e549267a7f5c010293f2be4845d8206
hallowf <alexandre.cortegaca@gmail.com>
2018-06-19 18:40:37 +0100

    Updated bottle server to handle websocket

--- bottle_server.py
+++ bottle_server.py
@@ -1,5 +1,11 @@
-from bottle import route, run, static_file
+from bottle import route, run, static_file, request, Bottle, abort
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+from geventwebsocket.handler import WebSocketHandler
 from Shell import shell_calls
+
+app = Bottle()
+
 
 # Reading html files
 def read_file(theFile):
@@ -9,54 +15,78 @@ def read_file(theFile):
     return html
 
 
+# The websocket
+@app.route("/websocket")
+def handle_websocket():
+    wsock = request.environ.get("wsgi.websocket")
+    if not wsock:
+        abort(400, "Expected a websocket request")
+
+    while True:
+        try:
+            message = wsock.receive()
+            wsock.send("Message Received")
+        except WebSocketError:
+            wsock.send(str(WebSocketError))
+
+
+
 # Serve static Js and Css files
-@route("/Web/CSS/Bootstrap/<filename>")
+@app.route("/Web/CSS/Bootstrap/<filename>")
 def serve_bootstrap(filename):
     return static_file(filename, root="Web/CSS/Bootstrap/")
 
-@route("/Web/Jquery/<filename>")
+@app.route("/Web/Jquery/<filename>")
 def serve_jquery(filename):
     return static_file(filename, root="Web/Jquery/")
 
-@route("/Web/Js/<filename>")
+@app.route("/Web/Js/<filename>")
 def serve_js(filename):
     return static_file(filename, root="Web/Js/")
 
 # Iframe for logs
-@route("/Web/html/outputs/<filename>")
+@app.route("/Web/html/outputs/<filename>")
 def serve_iframe(filename):
     shell_out = read_file("Web/html/outputs/{}".format(filename))
     return shell_out
 
 # Shell calls
-@route("/Shell/clean")
+@app.route("/Shell/clean")
 def clean_iframe():
     shell_calls.reset_soup()
 
-@route("/Shell/scripts/Start.sh")
+@app.route("/Shell/scripts/Start.sh")
 def start_install():
     shell_calls.call_start()
     #shell_calls.call_deps()
     #shell_calls.call_install()
 
 # Shell Tests
-@route("/Shell/tests/ls.sh")
+@app.route("/Shell/tests/ls.sh")
 def test_ls():
     shell_calls.call_ls()
 
-@route("/Shell/tests/cd.sh")
+@app.route("/Shell/tests/cd.sh")
 def test_cd():
     shell_calls.call_cd()
     shell_calls.call_bad_cd()
 
 
-
-
 # User Interface
-@route('/')
-@route('/home')
+@app.route('/')
+@app.route('/home')
 def hello():
     index = read_file("Web/index.html")
     return index
 
-run(host='localhost', port=8080, debug=True)
+
+server = WSGIServer(("127.0.0.1", 8080), app, handler_class=WebSocketHandler)
+
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    server.close()
+
+
+
+#run(host='localhost', port=8080, debug=True)
