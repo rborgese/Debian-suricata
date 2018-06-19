aadaeab7a7af460c338d8d590ed4cad7b14b53bc
hallowf <alexandre.cortegaca@gmail.com>
2018-06-19 18:41:07 +0100

    Working on websocket listener for browser side

--- Web/Js/shell_out.js
+++ Web/Js/shell_out.js
@@ -1,1 +1,13 @@
 
+function listen_websocket()
+{
+  var socket = new WebSocket("ws://127.0.0.1:8080/websocket")
+  const the_div = $("#shellout")
+  socket.onmessage =  function (event)
+  {
+    the_div.append("<p>" + event.data + "</p>")
+  }
+}
+
+
+listen_websocket()
