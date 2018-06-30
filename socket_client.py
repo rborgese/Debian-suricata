eb821132c36b0a05fe01f423876bb790880776df
hallowf <alexandre.cortegaca@gmail.com>
2018-06-30 16:42:53 +0100

    Removed unecessary socket client

--- socket_client.py
+++
@@ -1,9 +0,0 @@
-from websocket import create_connection
-
-
-def send_to(data):
-    ws = create_connection("ws://localhost:2890/websocket")
-    ws.send(data)
-    result =  ws.recv()
-    print ("Result '%s'" % result)
-    ws.close()
